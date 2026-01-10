85daf2acde12d5fd9b1c75898e4a7e45810acb08
devmuu <devmuu@proton.me>
2026-01-10 17:01:23 -0300

    settag.py refactored for more readability

--- scripts/.local/bin/settag.py
+++ scripts/.local/bin/settag.py
@@ -5,41 +5,53 @@
 # Description:   Set audio metadata
 # Software/Tool: python3.10.0/eyed3/toml
 # ==============================================================================
+
 """
 - mp3 files are biding with track names in a toml file
 - all files must begin with two digits track number
+
+- image priority:
+    1 - cover track defined by cover folder (if track is a list)
+    2 - cover file defined by album-year keys in list track (if track is a list)
+    3 - cover.jpg in file folder (same mp3 directory)
+    4 - cover.jpg in album artist folder (same place toml file)
+    5 - cover.jpg in artist folder (metadada folder)
+    6 - generic cover in music directory
 """
+
 import os
 import argparse
 import re
+from functools import lru_cache
 
 import toml
 import eyed3
 from eyed3.id3.frames import ImageFrame
 
-# variables
+# environment
 AUDIO_DIR = os.environ.get('AUDIO_DIR')
 METADATA_DIR = f'{AUDIO_DIR}/metadata'
 
-
 # args
 parser = argparse.ArgumentParser(description='Set music tags')
+parser.add_argument(
+    '-n',
+    '--dry-run',
+    action='store_true',
+    help='Simulate without writing',
+)
 parser.add_argument(
     '-d',
     '--data',
     type=str,
     default='info.toml',
-    metavar='data',
-    dest='f_data',
     help='Choose toml data file',
 )
 parser.add_argument(
     '-r',
     '--rename',
     type=str,
-    choices=['dash', 'dot', 'cover'],
-    metavar='rename',
-    dest='rename',
+    choices=['dash', 'dot'],
     help='Rename from metadata',
 )
 parser.add_argument(
@@ -48,162 +60,137 @@ parser.add_argument(
     type=str,
     default='mp3',
     choices=['mp3', 'mp4', 'wma'],
-    metavar='format',
-    dest='file_format',
     help='File format',
 )
 args = parser.parse_args()
 
+"""
+Begin Helpers
+"""
+
+# get right digits in track name
+def normalize_index(filename: str) -> str:
+    match = re.match(r'(\d+)', filename)
+    if not match:
+        raise ValueError("No track number")
+    return match.group(1).zfill(2)
+
+
+# get first artist in multi artist tag
+def first_artist(artist_field: str) -> str:
+    return artist_field.split(";")[0].strip()
+
+
+# to load toml file
+@lru_cache(maxsize=256)
+def load_toml(path: str):
+    return toml.load(path)
+
+"""
+End Helpers
+"""
 
 def get_image(data_path, track_list, artist, idx) -> str:
     """get correct image for track"""
-    # normalize track index if it is a list
-    idx_norm = int(idx)-1
 
-    # default image file
-    image = 'cover.jpg'
+    # check if cover index image exists in same mp3 file folder
+    single_img = f'covers/cover{idx}.jpg'
+    if os.path.isfile(single_img):
+        print(f"[COVER] Single Track Album image → {idx}")
+        return single_img
 
-    # check if covers directory exists
-    covers_test = os.path.isdir('covers')
-    if covers_test:
-        single_img = f'covers/cover{idx_norm+1}.jpg'
-        if os.path.isfile(single_img):
-            image = single_img
-
+    # block if track session is a list -> [[track]]
     if isinstance(track_list, list):
+        idx_norm = int(idx) - 1
+        track = track_list[idx_norm]
+
+        # verify if album and year keys exists in current item
         if 'album' and 'year' in track_list[idx_norm]:
             # search album cover in artist path
-            album_path = f'{track_list[idx_norm]["year"]} - {track_list[idx_norm]["album"]}'
+            album_path = f'{track["year"]} - {track["album"]}'
             album_cover = f'{METADATA_DIR}/Artists/{artist}/{album_path}/cover.jpg'
-            album_cover_test = os.path.isfile(album_cover)
 
-            if album_cover_test:
-                image = album_cover
+            if os.path.isfile(album_cover):
+                print(f"[COVER] Track Album image → {idx}")
+                return album_cover
 
         # test track has image key and search for covers folder in data_path
-        elif 'image' in track_list[idx_norm]:
-            track_cover = f'{data_path}/covers/cover{idx_norm+1}.jpg'
-            print(track_cover)
-
+        if 'image' in track:
+            track_cover = os.path.join(data_path, track["image"])
             if os.path.isfile(track_cover):
-                image = track_cover
+                print(f"[COVER] Single track album image → {idx}")
+                return track_cover
 
-    # image priority:
-    """
-        1 - cover.jpg in file folder (album thumb)
-        2 - cover.jpg in album artist metadada folder (album thumb)
-        3 - cover.jpg in artist folder (artist thumb)
-        4 - generic cover
-    """
+    # cover file in mp3 dir
+    if os.path.isfile('cover.jpg'):
+        print(f"[COVER] Directory cover → {idx}")
+        return 'cover.jpg'
 
-    # 1 test if cover.jpg exists in current or in covers directories
-    # if file does not exists, search in same path of info.toml
-    if not os.path.isfile(image):
-        album_cover = f'{data_path}/cover.jpg'
-        album_cover_test = os.path.isfile(album_cover)
+    # album cover in TOML folder
+    album_cover = f"{data_path}/cover.jpg"
+    if os.path.isfile(album_cover):
+        print(f"[COVER] Album cover → {idx}")
+        return album_cover
 
-        # 2 test if cover.jpg exists
-        if album_cover_test:
-            print(f'ALBUM COVER from metadata was set for track {idx}')
-            image = album_cover
-        else:
-            # get current artist metadata path
-            artist_cover = f'{METADATA_DIR}/Artists/{artist}/cover.jpg'
-            artist_cover_test = os.path.isfile(artist_cover)
+    # artist cover (first artist)
+    primary_artist = first_artist(artist)
+    artist_cover = f"{METADATA_DIR}/Artists/{primary_artist}/cover.jpg"
+    if os.path.isfile(artist_cover):
+        print(f"[COVER] Artist cover ({primary_artist}) → {idx}")
+        return artist_cover
 
-            # 3 search cover.jpg in artist metadata folder
-            if artist_cover_test:
-                print(f'DEFAULT ARTIST cover was set for track {idx}')
-                image = artist_cover
-            else:
-                # check some artist in multiple tag
-                multi_artist = artist.split("; ")
-                n_artists = len(multi_artist)
-
-                if n_artists > 1:
-                    multi_cover = f'{METADATA_DIR}/Artists/{artist}/cover.jpg'
-                    artist_cover_test = os.path.isfile(multi_cover)
-
-                    if not artist_cover_test:
-                        for name_artist in multi_artist:
-                            single_cover = f'{METADATA_DIR}/Artists/{name_artist}/cover.jpg'
-                            single_cover_test = os.path.isfile(single_cover)
-
-                            if single_cover_test:
-                                image = single_cover
-                                print(f'SINGLE artist cover was set  for track {idx}')
-                                break
-
-                        if not single_cover_test:
-                            image = f'{METADATA_DIR}/cover.jpg'
-                            print(f'GENERIC cover was set  for track {idx}')
-
-                        # check album cover in artist path
-                        if 'album' and 'year' in track_list[idx_norm] and single_cover_test:
-                            album_path = f'{track_list[idx_norm]["year"]} - {track_list[idx_norm]["album"]}'
-                            album_cover = f'{METADATA_DIR}/Artists/{name_artist}/{album_path}/cover.jpg'
-                            album_cover_test = os.path.isfile(album_cover)
-
-                            if album_cover_test:
-                                image = album_cover
-
-                else:
-                    # 4 if no one image found, set a generic cover from metadata dir
-                    print(f'GENERIC cover was set  for track {idx}')
-                    generic_cover = f'{METADATA_DIR}/cover.jpg'
-                    image = generic_cover
-    else:
-        # if cover.jpg or covers/cover<tracknumber>.jpg exists in mp3 directory
-        print(f'DEFAULT album cover was set for track {idx}')
-
-    return image
+    # generic in metadata music path
+    print(f"[COVER] Generic → {idx}")
+    return f"{METADATA_DIR}/cover.jpg"
 
 
 def write_tag(data_path, idx, file_name) -> None:
     """write tags"""
-    toml_data = toml.load(f'{data_path}/{f_data}')
-    album_artist = toml_data['stats']['artist']
-    album = toml_data['stats']['album']
-    disc_num = toml_data['stats']['discs'].split(',')
-    year = toml_data['stats']['year']
-    genre = toml_data['stats']['genre']
-    track_list = toml_data['tracks']
-    n_tracks = len(toml_data['tracks'])
+    toml_path = f"{data_path}/{f_data}"
+    toml_data = load_toml(toml_path)
 
-    # check if tracks info it is a list
-    if isinstance(track_list, list):
+    stats = toml_data["stats"]
+    tracks = toml_data["tracks"]
+
+    album_artist = stats['artist']
+    album = stats['album']
+    disc = stats['discs'].split(',')
+    year = stats['year']
+    genre = stats['genre']
+    total_tracks = len(tracks)
+
+    # check if tracks info is a list
+    if isinstance(tracks, list):
         idx_norm = int(idx) - 1
-        title = track_list[idx_norm]['title']
-        artist = track_list[idx_norm]['artist']
+        track = tracks[idx_norm]
+        title = track['title']
+        artist = track['artist']
     else:
-        title = track_list[f'track{idx}']
+        title = tracks[f'track{idx}']
         artist = album_artist
 
     # get image from get_image method
-    image = get_image(data_path, track_list, artist, idx)
+    image = get_image(data_path, tracks, artist, idx)
 
     tag_file = eyed3.load(file_name)
-
     if not tag_file.tag:
         tag_file.initTag()
 
+    # clear and set all tags
     tag_file.tag.clear()
-    tag_file.tag.track_num = (idx, n_tracks)
+    tag_file.tag.track_num = (int(idx), total_tracks)
     tag_file.tag.title = title
-    # replace ';' character to null values '\x00' to separate multi artist
-    tag_file.tag.artist = artist.replace("; ", '\x00')
-    tag_file.tag.album_artist = album_artist.replace("; ", '\x00')
     tag_file.tag.album = album
     tag_file.tag.recording_date = year
     tag_file.tag.release_date = year
-    # replace ';' character to null values '\x00' to separate genres
+
+    # replace ';' character to null values '\x00' to separate multi values
+    tag_file.tag.artist = artist.replace("; ", '\x00')
+    tag_file.tag.album_artist = album_artist.replace("; ", '\x00')
     tag_file.tag.genre = genre.replace("; ", '\x00')
 
     # tag disc number
-    if len(disc_num) > 1:
-        tag_file.tag.disc_num = (disc_num[0], disc_num[1])
-    else:
-        tag_file.tag.disc_num = (disc_num[0], disc_num[0])
+    tag_file.tag.disc_num = (int(disc[0]), int(disc[-1]))
 
     # clean tags with null value
     tag_file.tag.comments.set("null")
@@ -212,105 +199,98 @@ def write_tag(data_path, idx, file_name) -> None:
 
     # remove current images
     audioImageDescriptions = [audioImage.description for audioImage in tag_file.tag.images]
-
     for description in audioImageDescriptions:
         tag_file.tag.images.remove(description)
 
     # set image for front cover
-    tag_file.tag.images.set(ImageFrame.FRONT_COVER, open(image, 'rb').read(), 'image/jpeg')
+    tag_file.tag.images.set(
+        ImageFrame.FRONT_COVER,
+        open(image, 'rb').read(),
+        'image/jpeg'
+    )
     tag_file.tag.save()
+    print(f"[UPDATED] TAG → {os.path.basename(file_name)}")
 
 
-def rename_file(file_name, opt) -> None:
+def rename_file(file_name) -> None:
     """rename files"""
     # define correct separator and new filename
-    sep = ' - ' if opt == 'dash' else '. '
+    sep = " - " if args.rename == "dash" else ". "
     tag_file = eyed3.load(file_name)
     # bar '/' it's not accepted in filenames
     title_corretion = re.sub('/', '-', tag_file.tag.title)
-    # other characters to remove or change
+    # other characters to remove or change (keep this for more control)
     title_corretion = re.sub('\\?', '', title_corretion)
     title_corretion = re.sub('\\¿', '', title_corretion)
     title_corretion = re.sub('\\!', '', title_corretion)
     title_corretion = re.sub('\\*', '_', title_corretion)
-    set_name = f'{tag_file.tag.track_num[0]:02}{sep}{title_corretion}'
+    file_renamed = f'{tag_file.tag.track_num[0]:02}{sep}{title_corretion}'
 
-    # if new filename is the same current, except IOError
+    # only run a test
+    if args.dry_run:
+        print(f"[DRY-RUN] Would rename → {file_renamed}")
+        return
+
     try:
-        tag_file.rename(set_name)
-    except IOError as err:
-        print(f'[] {err}')
+        tag_file.rename(file_renamed)
+    except OSError as err:
+        print(f'[RENAMED]  {file_renamed} → exists and will not overwrite')
     else:
-        print(f'[] {set_name} -> renamed')
+        print(f'[RENAMED]  {file_renamed} → renamed')
 
 
 # define f_data from given arg
-if args.f_data is not None:
-    f_data = args.f_data
+if args.data is not None:
+    f_data = args.data
 
-if args.file_format is not None:
-    file_format = args.file_format
+
+def process_file(file_path):
+    try:
+        idx = normalize_index(os.path.basename(file_path))
+        pwd = os.path.dirname(file_path)
+
+        # when rename arg passed
+        if args.rename:
+            rename_file(file_path)
+            return
+
+        # skip next if in dry-run mode
+        if args.dry_run:
+            return
+
+        # local toml
+        if os.path.isfile(f"{pwd}/{f_data}"):
+            write_tag(pwd, idx, file_path)
+            return
+        # metadata mirror
+        meta = re.sub(f"{AUDIO_DIR}/music/(archived|disposable|main|review)", METADATA_DIR, pwd)
+        if os.path.isfile(f"{meta}/{f_data}"):
+            write_tag(meta, idx, file_path)
+            return
+
+        print(f"[SKIP] No TOML → {os.path.basename(file_path)}")
+
+    except Exception as e:
+        print(f"[ERROR] {os.path.basename(file_path)}: {e}")
 
 
 def main() -> None:
-    """main"""
-    pwd = os.getcwd()
+    # files list will save all music file path in current directory
+    files = []
 
-    # scan current directory
-    with os.scandir(pwd) as entries:
-        for entry in entries:
-            # get only current type files
-            if entry.name.endswith(f'.{file_format}'):
-                # get number track
-                # idx = entry.name.strip()[:2]
-                try:
-                    re.findall(r'\b\d+\b', entry.name.strip())[0]
-                except IndexError as err:
-                    idx = re.findall(r'\d+', entry.name.strip())[0]
-                else:
-                    idx = re.findall(r'\b\d+\b', entry.name.strip())[0]
+    # scan current dir and save filepath in a list
+    for f in os.listdir():
+        if f.endswith(f".{args.format}"):
+            files.append(os.path.abspath(f))
+    print(f"[SCAN] {len(files)} files allowed to work")
 
-                # add 0 left in track number to normalize with toml file pattern
-                if len(idx) < 2:
-                    idx = idx.rjust(2,'0')
-
-                # verify given arg
-                if args.rename:
-                    # send file_name and rename arg
-                    # files are renamed like own metadata
-                    # run settag.py before to pass all data in toml file
-                    rename_file(entry.name, args.rename)
-                else:
-                    # send file_name and f_data to write_tag
-                    data_test = os.path.isfile(f'{pwd}/{f_data}')
-
-                    # try find toml file in current folder, otherwise in metadata dir
-                    if data_test:
-                        write_tag(pwd, idx, entry.name)
-                    else:
-                        # try find toml file in metadata directory based on musics, archived or limbo directories
-                        meta_folder = re.sub(f'{AUDIO_DIR}/music/(main|archived|disposable|review)', f'{METADATA_DIR}', pwd)
-                        data_test = os.path.isfile(f'{meta_folder}/{f_data}')
-
-                        # if file exists, write tags
-                        if data_test:
-                            write_tag(meta_folder, idx, entry.name)
-                        else:
-                            # check last 2 folders in path for a pattern artist/album in Artists metadata
-                            p_album = os.path.basename(pwd)
-                            p_artist = os.path.basename(os.path.dirname(pwd))
-
-                            meta_folder = f'{METADATA_DIR}/Artists/{p_artist}/{p_album}'
-                            data_test = os.path.isfile(f'{meta_folder}/{f_data}')
-
-                            # if file exists, write tags, otherwise, break the loop
-                            if data_test:
-                                write_tag(meta_folder, idx, entry.name)
-                            else:
-                                print("No toml data file in metadata directory.")
-                                break
+    # process each file
+    for f in files:
+        process_file(f)
+    print("[DONE]")
 
 
 # run only in self, not in module
 if __name__ == '__main__':
     main()
+
